90003752c2ca3ebed247cd8784cac83c834ef72c
ssig33 <mail@ssig33.com>
2020-01-17 11:41:57 +0900

    Capture on Background script and notify to Popup via message.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,58 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Button, List, Checkbox, FormControlLabel } from '@material-ui/core';
-import { gyazo } from './gyazo';
-import { trim } from './trim';
-import ReactHotkeys from 'react-hot-keys';
-import { sendMessage, useStorage, useBlacklist } from './chrome';
-import { Capture } from './Capture';
 
-const useMessageFromContentScript = (callback) => {
-  useEffect(() => {
-    chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-      if (msg.type === 'res') {
-        callback(msg);
-      }
-      if (msg.type === 'rect') {
-        const { x, y, width, height } = msg;
-        chrome.tabs.captureVisibleTab(
-          null,
-          { format: 'png' },
-          async (dataUrl) => {
-            trim(dataUrl, x, y, width, height, (dataUrl) => {
-              callback({ ...msg, dataUrl });
-            });
-          }
-        );
-      }
-    });
-  }, []);
-};
+import ReactHotkeys from 'react-hot-keys';
+import { sendMessage, useStorage } from './chrome';
+import { Capture } from './Capture';
 
 const Popup = () => {
   const [list, setList] = useState([]);
   const [subs, setSubs] = useState(false);
-  const blacklist = useBlacklist();
-  const push = async (msg) => {
-    const { dataUrl, title, url } = msg;
-    const match = !!blacklist.filter((d) => url.match(d))[0];
-
-    const gyazoUrl = match
-      ? await gyazo({ image_url: dataUrl })
-      : await gyazo({ image_url: dataUrl, title, url });
-
-    const newList = Object.assign([], list);
-    newList.push(msg);
-    setList(newList);
-
-    const el = { ...msg, dataUrl: gyazoUrl.imageUrl, gyazo: gyazoUrl.gyazo };
-    chrome.storage.local.get(['list'], (result) => {
-      let list = [];
-      if (result.list) list = result.list;
-      list.unshift(el);
-      setList(Object.assign([], list));
-      chrome.storage.local.set({ list: list.slice(0, 20) });
-    });
-  };
 
   useStorage({ key: 'list' }, setList);
   useStorage({ key: 'subs' }, (subs) => {
@@ -60,7 +15,15 @@ const Popup = () => {
     capture(subs);
   });
 
-  useMessageFromContentScript(push);
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener((msg) => {
+      if (msg.type === 'listUpdated') {
+        chrome.storage.local.get(['list'], (result) =>
+          setList(result.list || [])
+        );
+      }
+    });
+  }, []);
 
   useEffect(() => {
     chrome.storage.local.set({ subs });
