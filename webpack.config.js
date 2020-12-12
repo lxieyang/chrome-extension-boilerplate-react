10979e3f60362170e1bf69ce283d13f13d1220d7
Ohad Cohen <ohadc.me@gmail.com>
2020-12-12 15:33:03 +0200

    manifest fixes

--- webpack.config.js
+++ webpack.config.js
@@ -122,15 +122,15 @@ var options = {
                 },
             ],
         }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: "src/pages/Content/content.styles.scss",
-                    to: path.join(__dirname, "build"),
-                    force: true,
-                },
-            ],
-        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: "src/pages/Content/content.styles.scss",
+        //             to: path.join(__dirname, "build"),
+        //             force: true,
+        //         },
+        //     ],
+        // }),
         // new HtmlWebpackPlugin({
         //   template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
         //   filename: 'newtab.html',
