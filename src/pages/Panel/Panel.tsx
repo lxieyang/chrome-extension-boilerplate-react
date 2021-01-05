1a97c68c71f5d96b9217e5c418aa874cf85520b2
GeekaholicLin <geekaholiclin@gmail.com>
2021-01-05 11:27:21 +0800

    add devtools support. Fixed #17

---
+++ src/pages/Panel/Panel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Panel.css';
+
+const Panel = (): JSX.Element => {
+  return (
+    <div className="container">
+      <h1>Dev Tools Panel</h1>
+    </div>
+  );
+};
+
+export default Panel;
