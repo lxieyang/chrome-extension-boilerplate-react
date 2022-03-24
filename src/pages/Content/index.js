828fa73409c672476789fb39ec185fc94d67eaa5
Jon Berry <jon@jonberry.co>
2022-03-24 14:50:09 -0400

    cleaning up template js

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,11 +1,3 @@
-import { printLine } from './modules/print';
-
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
-
-
 const staticVisionMapping = [
     { "right-temporally": 80 },
     { "right-down-temporally": 80 },
