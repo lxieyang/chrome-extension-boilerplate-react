0f4ac856b73ab6c1d2c669aeda0f6c11df28a046
Jon Berry <jon@jonberry.co>
2022-03-28 09:52:17 -0400

    additional hotfix annotation for deelopment.

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,7 @@
+// enable sass
 import './content.styles.scss';
 
-
+// ingest XML
 const staticVisionMapping = [
     { "right-temporally": 80 },
     { "right-down-temporally": 80 },
@@ -38,36 +39,37 @@ const staticVisionMapping = [
     { "left-lossType-no-light-perception": false }
 ];
 
-// instantiate input HTML object
+// instantiate hyperpop encapsulating HTML object
 var dbqXmlPicker = document.createElement("aside");
-// set input attribute type
-dbqXmlPicker.setAttribute('id',"hyperpop")
-// get first title on page
+// set id
+dbqXmlPicker.setAttribute('id', "hyperpop")
+// find first title on page
 var titleElement = document.getElementsByClassName('vision-calculator__title')[0];
-// append
-titleElement.after(dbqXmlPicker); 
+// append aside to DOM
+titleElement.after(dbqXmlPicker);
 
-
+// fetch contents of file picker UI
 fetch(chrome.runtime.getURL('/filepicker.html'))
-.then(response => response.text())
-.then(data => {
-    document.getElementById('hyperpop').innerHTML = data;
-    // other code
-    // eg update injected elements,
-    // add event listeners or logic to connect to other parts of the app
-}).catch(err => {
-    // handle error
-    console.log(Error)
-});
+    // parse response contents
+    .then(response => response.text())
+    // pass response contents for injection
+    .then(data => {
+        // inject
+        document.getElementById('hyperpop').innerHTML = data;
+        // other code
+        // eg update injected elements,
+        // add event listeners or logic to connect to other parts of the app
+    }).catch(err => {
+        // handle error
+        console.log(Error)
+    });
 
-
-
-
+// nastily drop in data.
 for (const mapping of staticVisionMapping) {
     var populatingFieldID = Object.keys(mapping)[0];
     var populatingValue = mapping[Object.keys(mapping)[0]]
     let populatingField = document.getElementById(populatingFieldID)
-    if(populatingField) {
+    if (populatingField) {
         populatingField.value = populatingValue
     }
 }
