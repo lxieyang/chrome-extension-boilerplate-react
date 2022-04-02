1cddd70bb6784763f97ecbdb6b796901df9c4c24
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:51:59 +0200

    Add comment for onUpdated chrome tab

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,6 +2,7 @@
 
 console.log('This is the background page.');
 
+// Example for listening to tab switching/loading
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     console.log('Tab finished loading', tabId, tab);
