1ccc0700dbba20553178a345c00ec01b044e59d8
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:31:51 +0200

    Add new tab

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,10 +3,10 @@ import { printLine } from './modules/print';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-// printLine("Using the 'printLine'");
+printLine("Using the 'printLine' at pages/Content/index.js");
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    console.log('Complete', tabId, tab);
+    console.log('Tab complete', tabId, tab);
     // do your things
     fetch('https://localshot:3000', {
       method: 'POST',
@@ -15,5 +15,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       .then((response) => response.json())
       .catch((e) => console.log(e))
       .then((data) => console.log(data));
+  } else {
+    console.log('Tab with tabId', tabId, 'not complete', tab);
   }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ if (fileSystem.existsSync(secretsPath)) {
 let options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
+    newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
     background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
@@ -160,6 +161,12 @@ let options = {
           force: true,
         },
       ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
+      filename: 'newtab.html',
+      chunks: ['newtab'],
+      cache: false,
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
