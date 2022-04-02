3aac38a0620e695fe8bc3fed659b4c59923ee789
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:40:21 +0200

    Remove fetch from Content, keep only background

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,18 +4,3 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' at pages/Content/index.js");
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == 'complete') {
-    console.log('Tab complete', tabId, tab);
-    // do your things
-    fetch('https://localshot:3000', {
-      method: 'POST',
-      body: JSON.stringify({ url: 'https://localhost/' }),
-    })
-      .then((response) => response.json())
-      .catch((e) => console.log(e))
-      .then((data) => console.log(data));
-  } else {
-    console.log('Tab with tabId', tabId, 'not complete', tab);
-  }
-});
