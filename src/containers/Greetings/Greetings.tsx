921e447412442cc9a7e6b0707bb8665a1255b681
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:51:00 +0200

    Remove fetch from Greetings

--- src/containers/Greetings/Greetings.tsx
+++ src/containers/Greetings/Greetings.tsx
@@ -1,19 +1,10 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 import icon from '../../assets/img/icon-128.png';
 
 export const Greetings = () => {
   const state = {
     name: 'dev',
   };
-
-  useEffect(() => {
-    async function loadURL() {
-      const res = await axios.get(`http://localhost:3000/`);
-      console.log('res', res);
-    }
-    loadURL();
-  });
 
   return (
     <div>
