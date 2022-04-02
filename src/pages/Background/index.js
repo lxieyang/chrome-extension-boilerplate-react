ee3fcd8851f38b0adb294c899ee89becf9994c15
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:50:07 +0200

    Remove fetch in background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -6,13 +6,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     console.log('Tab finished loading', tabId, tab);
     // do your things
-    fetch('https://localshot:3000', {
-      method: 'POST',
-      body: JSON.stringify({ url: 'https://localhost/' }),
-    })
-      .then((response) => response.json())
-      .catch((e) => console.log(e))
-      .then((data) => console.log(data));
   }
 });
 
