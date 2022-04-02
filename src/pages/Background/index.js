753621c69ba6e51e5418ffee4c4454d9a14425d1
Gustavo Rodríguez <gustavrod@gmail.com>
2022-04-02 13:48:31 +0200

    Remove JSX popup, add click example to popup

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -4,7 +4,7 @@ console.log('This is the background page.');
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    console.log('Tab finished loading, testing HTTP request', tabId, tab);
+    console.log('Tab finished loading', tabId, tab);
     // do your things
     fetch('https://localshot:3000', {
       method: 'POST',
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,27 +1,22 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import { Greetings } from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 export const Popup = () => {
-  const handleClick = () =>
-    fetch('https://localshot:3000', {
-      method: 'POST',
-      body: JSON.stringify({ url: 'https://localhost/' }),
-    })
-      .then((response) => response.json())
-      .catch((e) => console.log(e))
-      .then((data) => console.log(data));
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => setClicks(clicks + 1);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-          <button onClick={handleClick}>Test</button>
+          Edit <code>src/pages/Popup/Popup.tsx</code> and save to reload.
         </p>
+        <p>{clicks}</p>
+        <button onClick={handleClick}>Click me!</button>
         <a
           className="App-link"
           href="https://reactjs.org"
