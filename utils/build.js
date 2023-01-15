641d0f2bda191f8033e960512bec994d490ee629
lxieyang <lxieyang@umich.edu>
2023-01-14 22:02:14 -0500

    fix #123

--- utils/build.js
+++ utils/build.js
@@ -4,11 +4,23 @@ process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
 var webpack = require('webpack'),
-  config = require('../webpack.config');
+  path = require('path'),
+  fs = require('fs'),
+  config = require('../webpack.config'),
+  ZipPlugin = require('zip-webpack-plugin');
 
 delete config.chromeExtensionBoilerplate;
 
 config.mode = 'production';
+
+var packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+config.plugins = (config.plugins || []).concat(
+  new ZipPlugin({
+    filename: `${packageInfo.name}-${packageInfo.version}.zip`,
+    path: path.join(__dirname, '../', 'zip'),
+  })
+);
 
 webpack(config, function (err) {
   if (err) throw err;
