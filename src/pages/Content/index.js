867877a286b2a62a493301da25edd98da4a85198
Andrew Judson <ajskim@gmail.com>
2023-01-25 21:29:45 -0800

    Render React component in content script

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,19 @@
 import { printLine } from './modules/print';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Popup from './Popup';
+import './index.css';
+
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+// Create new div attached to body
+const newDiv = document.createElement('div');
+document.body.appendChild(newDiv);
+const root = createRoot(newDiv); // createRoot(container!) if you use TypeScript
+const popup = React.createElement(Popup, {});
+root.render(popup);
