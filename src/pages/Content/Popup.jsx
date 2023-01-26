43d32781204cafc85827054605a015be47d4b0fa
Andrew Judson <ajskim@gmail.com>
2023-01-25 22:04:52 -0800

    Call gpt3

--- src/pages/Content/Popup.jsx
+++ src/pages/Content/Popup.jsx
@@ -1,24 +1,50 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
+
 const Popup = () => {
+  const [userInput, setUserInput] = React.useState('Enter prompt here');
+  const [apiKey, setApiKey] = React.useState('Enter API key here');
+  const [result, setResult] = React.useState('Result will appear here');
+  function updateInputValue(evt) {
+    setUserInput(evt.target.value);
+  }
+  function updateApiKey(evt) {
+    setApiKey(evt.target.value);
+  }
+
+  async function callGPT3() {
+    // Call GPT3 with userInput
+    const url = `https://api.openai.com/v1/completions`;
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: userInput,
+            temperature:0.5
+        })
+    };
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+    const result = json.choices[0].text;
+    setResult(result);
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Content Script!
-        </a>
+        <textarea onChange={updateApiKey} value={apiKey}/>
+        <textarea onChange={updateInputValue} value={userInput}/>
+        <textarea value={result}/>
+        <button onClick={callGPT3}></button>
       </header>
     </div>
   );
