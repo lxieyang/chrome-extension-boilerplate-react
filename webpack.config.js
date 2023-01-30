48bd10b11a0febe0d94f219a3496dc0087c2937f
Andrew Judson <ajskim@gmail.com>
2023-01-30 11:34:54 -0800

    Change icons and description

--- webpack.config.js
+++ webpack.config.js
@@ -178,7 +178,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/img/icon-32.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
