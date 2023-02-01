2b04181e63821da89e59e8430ff3eebcbf0af260
Andrew Judson <ajskim@gmail.com>
2023-01-31 16:50:56 -0800

    Fix z-index on content container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        zIndex: {
+            // Max value is 2147483647
+            'max': '2147483647'
+        }
+      },
     },
     plugins: [],
     variants: {},
