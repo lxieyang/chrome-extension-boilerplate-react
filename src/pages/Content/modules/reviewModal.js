262dd475260c9ff163c4fc31d0c5943aec5e4077
KunalC <sanskarchaurasia123@gmail.com>
2023-05-07 12:43:29 +0530

    add: Reviews

--- src/pages/Content/modules/reviewModal.js
+++ src/pages/Content/modules/reviewModal.js
@@ -174,6 +174,222 @@ contentTop.appendChild(reviewsDiv5);
 bodyBody.appendChild(contentDiv);
 bodyBody.appendChild(productDetails);
 
+//Date
+const filter = document.createElement('div');
+filter.setAttribute('class', 'jvntzP');
+
+const mainDiv = document.createElement('div');
+mainDiv.setAttribute('class', 'sc-jSUnYo kiqpP');
+
+const dateRangeDiv = document.createElement('div');
+dateRangeDiv.setAttribute('class', 'sc-cXKCzB bSssay');
+
+const dateInputDiv = document.createElement('div');
+dateInputDiv.setAttribute('data-testid', 'control-dateInput');
+dateInputDiv.setAttribute('class', 'sc-fQhdLw ktAmda');
+dateInputDiv.setAttribute('aria-expanded', 'false');
+
+const dateInputWrapperDiv = document.createElement('div');
+dateInputWrapperDiv.setAttribute('class', 'sc-httYss fXWfqw sc-cvAOhJ eTjXPv');
+
+const dateInputFieldDiv = document.createElement('div');
+dateInputFieldDiv.setAttribute('class', 'sc-dkIXZx sc-fWPcWZ gtgZQm bpAFPj');
+
+const dateInput = document.createElement('input');
+dateInput.setAttribute('placeholder', 'Select the date range');
+dateInput.setAttribute('class', 'sc-XhUvE sc-jXktde kkDNaA');
+dateInput.setAttribute('value', '');
+
+dateInputFieldDiv.appendChild(dateInput);
+dateInputWrapperDiv.appendChild(dateInputFieldDiv);
+dateInputDiv.appendChild(dateInputWrapperDiv);
+dateRangeDiv.appendChild(dateInputDiv);
+
+const filterDiv = document.createElement('div');
+filterDiv.setAttribute('class', 'sc-fUfnfA ckHJBG');
+
+const filterDropdownDiv = document.createElement('div');
+filterDropdownDiv.setAttribute('class', 'sc-flPZYa lerQSN');
+
+const filterBtnDiv = document.createElement('div');
+filterBtnDiv.setAttribute('data-open', 'false');
+filterBtnDiv.setAttribute('tabindex', '0');
+filterBtnDiv.setAttribute('class', 'sc-gsxFXZ hNeyOA');
+filterBtnDiv.setAttribute('aria-expanded', 'false');
+
+const filterTextDiv = document.createElement('div');
+filterTextDiv.setAttribute('class', 'sc-dmaBdM jMlZvI');
+
+const filterText = document.createElement('span');
+filterText.setAttribute('class', 'sc-kIjwQX hlNbkn');
+filterText.innerHTML = 'Select filters';
+
+filterTextDiv.appendChild(filterText);
+filterBtnDiv.appendChild(filterTextDiv);
+filterDropdownDiv.appendChild(filterBtnDiv);
+filterDiv.appendChild(filterDropdownDiv);
+
+const clearBtn = document.createElement('div');
+clearBtn.setAttribute('class', 'sc-dWddBi jSmkTg');
+clearBtn.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="svg-inline--fa fa-star sc-lfnXyL fhmLEe" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M287.9 0C297.1 0 305.5 5.25 309.5 13.52L378.1 154.8L531.4 177.5C540.4 178.8 547.8 185.1 550.7 193.7C553.5 202.4 551.2 211.9 544.8 218.2L433.6 328.4L459.9 483.9C461.4 492.9 457.7 502.1 450.2 507.4C442.8 512.7 432.1 513.4 424.9 509.1L287.9 435.9L150.1 509.1C142.9 513.4 133.1 512.7 125.6 507.4C118.2 502.1 114.5 492.9 115.1 483.9L142.2 328.4L31.11 218.2C24.65 211.9 22.36 202.4 25.2 193.7C28.03 185.1 35.5 178.8 44.49 177.5L197.7 154.8L266.3 13.52C270.4 5.249 278.7 0 287.9 0L287.9 0zM287.9 78.95L235.4 187.2C231.9 194.3 225.1 199.3 217.3 200.5L98.98 217.9L184.9 303C190.4 308.5 192.9 316.4 191.6 324.1L171.4 443.7L276.6 387.5C283.7 383.7 292.2 383.7 299.2 387.5L404.4 443.7L384.2 324.1C382.9 316.4 385.5 308.5 391 303L476.9 217.9L358.6 200.5C350.7 199.3 343.9 194.3 340.5 187.2L287.9 78.95z"></path></svg>';
+
+const applyBtn = document.createElement('button');
+applyBtn.setAttribute('class', 'sc-dWddBi NZXZk');
+applyBtn.innerHTML = 'Apply Filters';
+
+mainDiv.appendChild(dateRangeDiv);
+mainDiv.appendChild(filterDiv);
+mainDiv.appendChild(clearBtn);
+mainDiv.appendChild(applyBtn);
+filter.appendChild(mainDiv);
+
+//Overview
+
+  const overviewDiv = document.createElement('div');
+  overviewDiv.setAttribute('class', 'gnaJEW')
+
+  const overviewSplitDiv = document.createElement('div');
+  overviewSplitDiv.setAttribute('class', 'gxxFEr')
+
+  const overviewLeftDiv = document.createElement('div');
+  overviewLeftDiv.setAttribute('class', 'jBOLsP')
+
+  const overviewTableDiv = document.createElement('div');
+  overviewTableDiv.setAttribute('class', 'hhdZCl')
+
+  const tableWrapper = document.createElement('div');
+  tableWrapper.setAttribute('class', 'jWPjfx')
+
+  const tableWrapper2 = document.createElement('div');
+  tableWrapper2.setAttribute('class', 'cfrHHF')
+
+  const tableGrids = document.createElement('div');
+  tableGrids.setAttribute('class', 'jJAKYt')
+
+  const column1 = document.createElement('div')
+  column1.setAttribute('style', 'display: contents;')
+  const column2 = document.createElement('div')
+  column2.setAttribute('style', 'display: contents;')
+  const column3 = document.createElement('div')
+  column3.setAttribute('style', 'display: contents;')
+  const column4 = document.createElement('div')
+  column4.setAttribute('style', 'display: contents;')
+
+  //Rating
+  const rating = document.createElement('div')
+  rating.setAttribute('class', 'dZrlxP')
+
+  const ratingName = document.createElement('div')
+  ratingName.setAttribute('class', 'bimpkv')
+  ratingName.innerHTML= 'Rating'
+
+  //Rating count
+  const ratingCount = document.createElement('div')
+  ratingCount.setAttribute('class', 'fRhIzs')
+
+  const ratingCountDiv = document.createElement('div')
+  ratingCountDiv.setAttribute('class', 'eUFYmb')
+
+  const ratingCountName = document.createElement('div')
+  ratingCountName.setAttribute('class', 'uXEUb')
+  ratingCountName.innerHTML= 'Rating Count'
+
+  //Distribution
+
+  const distribution = document.createElement('div')
+  distribution.setAttribute('class', 'fRhIzs')
+
+  const distributionName = document.createElement('div')
+  distributionName.setAttribute('class', 'eUFymb')
+  distributionName.innerHTML= 'Distribution'
+
+  //See All
+
+  const seeAll = document.createElement('div')
+  seeAll.setAttribute('class', 'fRhIzs')
+
+  const seeAllDiv = document.createElement('div')
+  seeAllDiv.setAttribute('class', 'GtsQR')
+
+  rating.appendChild(ratingName);
+  column1.appendChild(rating);
+  distribution.appendChild(distributionName);
+  column3.appendChild(distribution);
+  ratingCountDiv.appendChild(ratingCountName);
+  ratingCount.appendChild(ratingCountDiv);
+  column2.appendChild(ratingCount);
+  seeAll.appendChild(seeAllDiv);
+  column4.appendChild(seeAll);
+
+  tableGrids.appendChild(column1);
+  tableGrids.appendChild(column2);
+  tableGrids.appendChild(column3);
+  tableGrids.appendChild(column4);
+
+  tableWrapper2.appendChild(tableGrids);
+  tableWrapper.appendChild(tableWrapper2);
+  overviewTableDiv.appendChild(tableWrapper);
+  overviewLeftDiv.appendChild(overviewTableDiv);
+  overviewSplitDiv.appendChild(overviewLeftDiv);
+  overviewDiv.appendChild(overviewSplitDiv);
+  bodyBody.appendChild(overviewDiv);
+
+
+  const overviewDiv2 = document.createElement('div');
+  overviewSplitDiv.appendChild(overviewDiv2);
+
+  const overviewRightDiv = document.createElement('div');
+  overviewRightDiv.setAttribute('class','epbpWv');
+
+  overviewDiv2.appendChild(overviewRightDiv);
+
+  const overviewTopTitle = document.createElement('div');
+  overviewTopTitle.setAttribute('class','bgGCiT');
+  overviewTopTitle.innerHTML='Top Helpful Review';
+
+  const overviewReviewDiv = document.createElement('div');
+  overviewReviewDiv.setAttribute('class','gtKjyK');
+
+  overviewRightDiv.appendChild(overviewTopTitle);
+  overviewRightDiv.appendChild(overviewReviewDiv);
+
+  const overviewReviews = document.createElement('div');
+  overviewReviews.setAttribute('class','iynKDA');
+
+  overviewReviewDiv.appendChild(overviewReviews);
+
+  const overviewReviewTitle = document.createElement('div');
+  overviewReviewTitle.setAttribute('class','ixxElA');
+
+  overviewReviews.appendChild(overviewReviewTitle);
+
+  const overviewReviewTitleText = document.createElement('a');
+  overviewReviewTitleText.setAttribute('class','udqtB');
+  overviewReviewTitleText.innerHTML='Over all Review of the Redmi 12 C phone.'
+
+  overviewReviewTitle.appendChild(overviewReviewTitleText);
+
+  const overviewReviewUser = document.createElement('div');
+  overviewReviewUser.setAttribute('class','ixxElA');
+
+  overviewReviews.appendChild(overviewReviewUser);
+
+  const overviewReviewUserDetail = document.createElement('div');
+  overviewReviewUserDetail.setAttribute('class','fpVYlo');
+  overviewReviewUserDetail.innerHTML='By Rohit, Reviewed in India 🇮🇳 on 25 April 2023'
+
+  overviewReviewUser.appendChild(overviewReviewUserDetail);
+
+  const overviewReviewDetail = document.createElement('div');
+  overviewReviewDetail.setAttribute('class','kqSYrC');
+  overviewReviewDetail.innerHTML="PROS:-Decent camera at this price range.Good Speed.The display quality is excellent.The fingerprint is working properly &amp; smoothly.smooth touch screen.Excellent battery backup.CONS:-Back Panel quality bit poor.Sound a bit low (enough for normal use).To many inbuilt (unnecessary) apps.They didn't provide a back cover for the phone.Turbo Charger or C type is not provided."
+
+  overviewReviews.appendChild(overviewReviewDetail);
+
+  const overviewReviewHelpful = document.createElement('div');
+  overviewReviewHelpful.setAttribute('class','ekqgTH');
+  overviewReviewHelpful.innerHTML='35 people found this helpful'
+  overviewReviews.appendChild(overviewReviewHelpful);
 
 
     reviewsType1.onclick = async function () {
