988245fb9f35378bc57d45e375e3a58b12de00c6
mukul agarwal <mukulagarwa@adobe.com>
2023-05-12 23:22:36 +0530

    fixes

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -5,78 +5,11 @@ import Greetings from '../../containers/Greetings/Greetings';
 import { Box, Button, Switch, Typography } from '@mui/material';
 
 const Popup = () => {
-  let profitabiltyUseCountKey = 'dv_pc_use_count';
-  let reviewUseCountKey = 'dv_review_use_count';
-  async function profitability_modal() {
-    // chrome.storage.local.remove(constants.authTokenKey);
-    // chrome.storage.local.remove(profitabiltyUseCountKey);
-    let authToken = await getAuthToken();
-    let useCount = await chrome.storage.local.get(profitabiltyUseCountKey);
-    if (
-      (authToken && authToken[constants.authTokenKey]) ||
-      !useCount[profitabiltyUseCountKey] ||
-      useCount[profitabiltyUseCountKey] < 2
-    ) {
-      let currentUseCount = useCount[profitabiltyUseCountKey]
-        ? useCount[profitabiltyUseCountKey] + 1
-        : 1;
-      chrome.storage.local.set({
-        [profitabiltyUseCountKey]: currentUseCount,
-      });
-      chrome.runtime.sendMessage({ message: 'profitability_modal' });
-    } else {
-      // generate random string and store in local storage
-      let referrerIdValue = await getReferrerIdKey();
-      let uuid = referrerIdValue
-        ? referrerIdValue[constants.referrerIdKey]
-        : uuidv4();
-      if (!referrerIdValue) {
-        chrome.storage.local.set({ [constants.referrerIdKey]: uuid });
-      }
-
-      chrome.tabs.create({
-        url: `${constants.API_URL}login?referrer=extension&referrerId=${uuid}`,
-        active: true,
-      });
-    }
+  function profitability_modal() {
+    chrome.runtime.sendMessage({ message: 'profitability_modal' });
   }
-  async function review_modal() {
-    // let useCount = await chrome.storage.local.get(reviewUseCountKey);
-    // if (useCount[reviewUseCountKey] >= 2) {
-    //   // show login modal, // clicking on login redirect to login page
-    //   chrome.tabs.create({ url: 'https://www.datavio.co/login', active: true });
-    //   return;
-    // } else {
-    //   let currenUseCount = useCount[reviewUseCountKey]
-    //     ? useCount[reviewUseCountKey] + 1
-    //     : 1;
-    //   chrome.storage.local.set({
-    //     [reviewUseCountKey]: currenUseCount,
-    //   });
-    //   chrome.runtime.sendMessage({ message: 'review_modal' });
-    // }
-  }
-
-  async function refresh_token() {
-    let referrerIdValue = await getReferrerIdKey();
-    if (referrerIdValue) {
-      referrerIdValue = referrerIdValue[constants.referrerIdKey];
-      // make  request to refresh token
-      try {
-        const response = await fetch(
-          `${constants.PRODUCT_API_URL}extension/getToken?referrerId=${referrerIdValue}`
-        );
-        const data = await response.json();
-        if (data && data['loginToken']) {
-          chrome.storage.local.set({
-            [constants.authTokenKey]: data['loginToken'],
-          });
-        }
-      } catch (error) {
-        alert('login failed');
-      }
-    }
-    return;
+  function review_modal() {
+    chrome.runtime.sendMessage({ message: 'review_modal' });
   }
   return (
     <Box
