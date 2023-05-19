6675e1bcdd5d91e36d525226ef2d7e28ebac94e9
Chen Reuven <chen.rvn@gmail.com>
2023-05-19 21:37:04 +0300

    feat: first commit

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import Popup from './Popup';
 import './index.css';
 
+// Root
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<Popup />);
