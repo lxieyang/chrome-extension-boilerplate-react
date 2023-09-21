9038f4fd877f146588a8a14555fa0fac5d70e1b4
meleshun <meleshun@gmail.com>
2023-09-21 15:23:50 +0200

    feat: Add Side Panel API support - Description: Added side_panel configuration, permissions, and Sidepanel entry point.

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,7 @@ var options = {
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
     devtools: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.js'),
     panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
+    sidepanel: path.join(__dirname, 'src', 'pages', 'Sidepanel', 'index.jsx'),
   },
   chromeExtensionBoilerplate: {
     notHotReload: ['background', 'contentScript', 'devtools'],
@@ -212,6 +213,12 @@ var options = {
       template: path.join(__dirname, 'src', 'pages', 'Panel', 'index.html'),
       filename: 'panel.html',
       chunks: ['panel'],
+      cache: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'pages', 'Sidepanel', 'index.html'),
+      filename: 'sidepanel.html',
+      chunks: ['sidepanel'],
       cache: false,
     }),
   ].filter(Boolean),
