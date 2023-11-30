1f92bbbca072c99100a705f04dcd1eae3dbe9f11
Viet Hoang <ngviethoang0212@gmail.com>
2023-11-30 21:19:38 +0700

    add secrets

--- src/pages/Newtab/Newtab.tsx
+++ src/pages/Newtab/Newtab.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Newtab = () => {
-  return <div className="w-3/4 mx-auto"></div>;
+  return <div className="w-3/4 mx-auto">hello</div>;
 };
 
 export default Newtab;
