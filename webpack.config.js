4fdd3526b76fa84643d404630a9181f5bfaba561
Ambushfall <lukathetormentor@gmail.com>
2024-02-27 01:40:49 +0100

    Add Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,9 @@ var options = {
             options: {
               sourceMap: true,
             },
-          },
+          }, {
+            loader: 'postcss-loader'
+          }
         ],
       },
       {
